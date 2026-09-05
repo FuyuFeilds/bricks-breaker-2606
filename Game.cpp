cb2b97d4405ca766fb16f64129d2a1b29ba9f1ee
Kaden Smith <KJSmith7@student.fullsail.edu>
2026-09-04 20:12:10 -0700

    todo #7 complete

--- Game.cpp
+++ Game.cpp
@@ -126,4 +126,8 @@ void Game::CheckCollision()
 	}
 
 	// TODO #7 - If ball touches bottom of window, pause ball and display (render) defeat text with R to reset
+	if (ball.y_position >= WINDOW_HEIGHT - 1) 
+	{
+		ball.moving = false;
+	}
 }
